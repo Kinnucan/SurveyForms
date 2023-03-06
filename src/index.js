442e89e21a54c8f115ed227230fde90813d3ebb9
Kinnucan <ryan.kinnucan@gtt.com>
2023-03-06 08:19:36 -0600

    Reverted to Netlify urls

--- src/index.js
+++ src/index.js
@@ -10,28 +10,9 @@ root.render(
     domain="dev-ubwj2dc66hp08axm.us.auth0.com"
     clientId="7nEiUxXbBsresyr637WYTS28sciyUQgx"
     authorizationParams={{
-      redirect_uri: "https://127.0.0.1:3000/forms",
+      redirect_uri: "https://precious-kangaroo-784609.netlify.app/forms",
     }}
   >
     <App />
   </Auth0Provider>
 );
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { Auth0Provider } from "@auth0/auth0-react";
-// import App from "./App/App";
-// import "./index.css";
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <Auth0Provider
-//     domain="dev-ubwj2dc66hp08axm.us.auth0.com"
-//     clientId="7nEiUxXbBsresyr637WYTS28sciyUQgx"
-//     authorizationParams={{
-//       redirect_uri: "https://precious-kangaroo-784609.netlify.app/forms",
-//     }}
-//   >
-//     <App />
-//   </Auth0Provider>
-// );
